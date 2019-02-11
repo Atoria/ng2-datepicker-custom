a402e60be81102af57cfa2e957c7ab13f1436771
Andria Toria <toria.andria@gmail.com>
2019-02-11 17:05:50 +0400

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+import { DateModel, DatePickerOptions } from '../ng2-datepicker/ng2-datepicker.component';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+})
+export class AppComponent {
+  date: DateModel;
+  options: DatePickerOptions;
+
+  constructor() {
+    this.options = new DatePickerOptions();
+  }
+}
